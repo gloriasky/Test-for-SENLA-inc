5a7f62c528ebcb93ede098150e8c654945598082
Ксения Володько <singer.ksu@gmail.com>
2018-05-29 13:47:18 +0300

    zad1 - done

--- src/com/company/Methods.java
+++ src/com/company/Methods.java
@@ -4,14 +4,31 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Methods {
-        public static void menu(){
-            System.out.println("Tasks were made by Ksenia Volodko" +
-                    "\nWhat task would you like to check?");
+        public static void menu() {
             Scanner scanner = new Scanner(System.in);
-            int i = scanner.nextInt();
-            switch(i){
-                case 1: task1();
-            }
+            Scanner scr = new Scanner(System.in);
+            System.out.println("Tasks were made by Ksenia Volodko");
+            String answer;
+            do {
+                System.out.println("What task would you like to check?");
+                int i = scanner.nextInt();
+                switch (i) {
+                    case 1:task1(); break;
+                    case 2: break;
+                    case 3: break;
+                    case 4: break;
+                    case 5:break;
+                    case 6: break;
+                    case 7: break;
+                    case 8: break;
+                    case 9: break;
+                    case 10: break;
+                    default:
+                        System.out.println("Are you sure that task exist?");
+                }
+                System.out.println("Would you like to check anything else?");
+                answer = scr.nextLine();
+            }while(answer.equalsIgnoreCase("YES") || answer.equalsIgnoreCase("ДА"));
         }
         private static void task1(){
             try {
